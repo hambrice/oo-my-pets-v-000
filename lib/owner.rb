7a53cce8e3a56169c5915092ee65560a2486acae
Brad Hambrice <hambrice@uab.edu>
2018-01-10 03:52:39 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -6,7 +6,16 @@ class Owner
     @pets = {fishes: [], cats: [], dogs: []}
     @@owners << self 
   end
+  
+  def all 
+    @@owners
+  end 
+  
   def count 
     @@owners.size 
   end 
+  
+  def reset_all
+    @@owners.clear
+  end
 end
