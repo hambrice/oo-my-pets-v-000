6ba2e87ff8a35ab2b95d732451bd30e9dbe3f7b9
Brad Hambrice <hambrice@uab.edu>
2018-01-10 03:51:16 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,12 @@
 class Owner
-  # code goes here
+  attr_accessor :pets 
+  @@owners = []
+  
+  def initialize
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@owners << self 
+  end
+  def count 
+    @@owners.size 
+  end 
 end
